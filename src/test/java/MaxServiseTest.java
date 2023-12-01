5c486f98f9555a3a58d206b550e857490c9c5ef6
YuriS <yuri-s@list.ru>
2023-12-01 17:47:37 +0300

    githab MaxServiseTest 01.12.23

--- src/test/java/MaxServiseTest.java
+++ src/test/java/MaxServiseTest.java
@@ -5,12 +5,24 @@ public class MaxServiseTest {
 
 
     @Test
-    public void shouldFindMax() {
+    public void shouldFindMaxA() {
         MaxServise servise = new MaxServise();
         int a = 5;
         int b = 3;
 
         int expected = a;
+        int actual = servise.max(a, b);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindMaxB() {
+        MaxServise servise = new MaxServise();
+        int a = 3;
+        int b = 5;
+
+        int expected = b;
         int actual = servise.max(a, b);
 
         Assertions.assertEquals(expected, actual);
