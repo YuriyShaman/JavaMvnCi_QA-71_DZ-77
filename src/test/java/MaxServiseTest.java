cf23052db686dcc283658cbf7be9d4089aec30cb
YuriS <yuri-s@list.ru>
2023-12-27 18:15:44 +0300

    Correct push commit

--- src/test/java/MaxServiseTest.java
+++ src/test/java/MaxServiseTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 public class MaxServiseTest {
 
 
-    @Test !
+    @Test
     public void shouldFindMaxA() {
         MaxServise servise = new MaxServise();
         int a = 5;
